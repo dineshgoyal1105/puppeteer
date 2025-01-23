2a7f1ed44f096e6d1f33dde61a6953fb8d0d8c62
dineshgoyal1105 <dinesh.goyal1105@gmail.com>
2025-01-23 13:29:26 +0530

    pdf maker

--- pdf.js
+++ pdf.js
@@ -1,22 +1,60 @@
-const puppeteer = require("puppeteer");
+// const puppeteer = require("puppeteer");
+// const fs = require('fs');
+// const path = require('path');
+// const data = require('./index2.html')
+// async function downloadPdfFromUrl(url, outputPath) {
+//     const browser = await puppeteer.launch({ headless: "new" });
+//     const page = await browser.newPage();
+//
+//     // Navigate to the specified URL
+//     await page.goto(url, { waitUntil: "networkidle0" });
+//
+//     // Generate PDF from the page content
+//     await page.pdf({ path: outputPath, format: "A4" });
+//
+//     // Close the browser
+//     await browser.close();
+// }
+//
+// const targetUrl = data;
+// const outputFile = "downloaded_page3.pdf";
+//
+// downloadPdfFromUrl(targetUrl, outputFile)
+//     .then(() => console.log(`PDF downloaded successfully at: ${outputFile}`))
+//     .catch((error) => console.error("Error:", error));
 
-async function downloadPdfFromUrl(url, outputPath) {
-    const browser = await puppeteer.launch({ headless: "new" });
+import puppeteer from 'puppeteer';
+import { readFileSync } from 'fs';
+
+
+(async () => {
+    console.log(puppeteer.defaultBrowserRevision);
+
+    // Start the browser
+    const browser = await puppeteer.launch();
+
+    // Open a new blank page
     const page = await browser.newPage();
 
-    // Navigate to the specified URL
-    await page.goto(url, { waitUntil: "networkidle0" });
+    // Set screen size
+    await page.setViewport({ width: 1920, height: 1080 });
 
-    // Generate PDF from the page content
-    await page.pdf({ path: outputPath, format: "A4" });
+    // Open the index.html file
+    const htmlContent = readFileSync('./index2.html', 'utf-8');
+
+    // Now we use setContent instead of goto
+    await page.setContent(htmlContent, { waitUntil: 'networkidle0' });
+
+    // Use screen CSS instead of print
+    await page.emulateMediaType('screen');
+
+    // Render the PDF
+    const pdf = await page.pdf({
+        path: 'render.pdf', // Output the result in a local file
+        printBackground: true,
+        format: 'A4',
+    });
 
     // Close the browser
     await browser.close();
-}
-
-const targetUrl = "http://localhost:63342/puppetter/index2.html?_ijt=og9rorig9cotnji85furbkl24g";
-const outputFile = "downloaded_page3.pdf";
-
-downloadPdfFromUrl(targetUrl, outputFile)
-    .then(() => console.log(`PDF downloaded successfully at: ${outputFile}`))
-    .catch((error) => console.error("Error:", error));
+})();
